29242d1eb9f2426d5691c0ce47147d675515689f
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-01-16 15:56:14 +0100

    job 5 done

---
+++ job05/script.js
@@ -0,0 +1,10 @@
+function afficherjoursemaines(){
+    const joursemaines = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"]
+
+    for(let i=0;i<joursemaines.length;i++){
+        console.log(joursemaines[i]);
+    }
+}
+
+
+console.log(afficherjoursemaines());
