170c57516e6e69d873adeafd33bc7485410b1694
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-01-17 10:43:06 +0100

    file moove

---
+++ jour02/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+<body>
+<article id="citation">La vie a beaucoup plus d’imagination que nous</article>
+
+<button id="button" onclick="citation()">Afficher la citation</button>
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,7 @@
+
+
+  function citation() {
+    var citation = document.getElementById("citation").innerHTML;
+    console.log(citation);
+  }
+
