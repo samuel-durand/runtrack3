d51bc09bbc0dbede33fafc03d9b20eee0740b45b
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-01-18 15:00:32 +0100

    job01 done and jqeury add in folder

---
+++ jour03/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>job01</title>
+    <script src="../jquery-3.6.3.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+
+    <p>“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+ensuite on prie.”</p>     
+
+    <button id="afficher">afficher</button>
+    <button id="cacher">cacher</button>
+    
+</body>
+</html>
