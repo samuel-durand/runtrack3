a5753d93475599c5cfdb38f8bf6d39abb96459b9
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-01-24 11:26:39 +0100

    job01 done

---
+++ jour08/job01/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>job01</title>
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="">Acceuil</a></li>
+            <li><a>Inscription</a></li>
+            <li><a>Connexion</a></li>
+            <li><a>Rechercher</a></li>
+        </ul>
+    </header>
+
+        <form>
+            <label for="name">entrer votre nom</label><br>
+            <input type="text" placeholder="nom" required><br>
+
+            <label for="prénom">votre prénom</label><br>
+            <input type="text" placeholder="prénom" required><br>
+
+            <label for="adresse">votre adresse</label><br>
+            <input type="text" placeholder="adresse" required><br>
+
+            <label for="password">votre mot de pass</label><br>
+            <input type="password" placeholder="mot de pass" required><br>
+
+            <input type="radio" name="homme" value="homme">
+            <label for="homme">Homme</label><br>
+
+            <input type="radio" name="Femme" value="Femme">
+            <label for="Femme">Femme</label><br>
+
+            <label>vos passions ! :</label>
+
+            <input type="checkbox">
+            <label for="informatique">informatique</label>
+
+            <input type="checkbox">
+            <label for="voyage">voyage</label>
+
+            <input type="checkbox">
+            <label for="sport">sport</label>
+            <br>
+            <input type="submit" value="envoyer !">
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
+
