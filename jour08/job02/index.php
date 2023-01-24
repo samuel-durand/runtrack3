c6091f285e60220de4ca8d5668c7b84b31cdcdd8
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-01-24 16:04:18 +0100

    job 02

---
+++ jour08/job02/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>job02</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style type="text/tailwindcss"></style>
+
+  </style>
+</head>
+<body>
+    <header>
+        <nav class="flex justify-evenly bg-gradient-to-r from-purple-500 to-pink-500 w-100 h-16 text-white" >
+            <a class="mt-4 mx-8 cursor-pointer ">Acceuil</a>
+            <a class="mt-4 mx-8 cursor-pointer" >Inscription</a>
+            <a class="mt-4 mx-8 cursor-pointer">Connexion</a>
+            <a class="mt-4 mx-8 cursor-pointer">Rechercher</a>
+        </nav>
+ 
+    </header>
+
+        <form >
+            <label for="name">entrer votre nom</label><br>
+            <input type="text" placeholder="nom" required><br>
+
+            <label for="prénom">votre prénom</label><br>
+            <input type="text" placeholder="prénom" required><br>
+
+            <label for="adresse">votre adresse</label><br>
+            <input type="text" placeholder="adresse" required><br>
+
+            <label for="password">votre mot de pass</label><br>
+            <input type="password" placeholder="mot de pass" required><br>
+
+            <input type="radio" name="homme" value="homme">
+            <label for="homme">Homme</label><br>
+
+            <input type="radio" name="Femme" value="Femme">
+            <label for="Femme">Femme</label><br>
+
+            <label>vos passions ! :</label>
+
+            <input type="checkbox">
+            <label for="informatique">informatique</label>
+
+            <input type="checkbox">
+            <label for="voyage">voyage</label>
+
+            <input type="checkbox">
+            <label for="sport">sport</label>
+            <br>
+            <input type="submit" value="envoyer !">
+            <br>
+
+    <footer class="flex items-center justify-evenly	 bg-gradient-to-r from-purple-500 to-pink-500  absolute inset-x-0 bottom-0 h-16 text-white ">
+        <a >Acceuil</a>
+        <a >Inscription</a>
+        <a>Connexion</a>
+        <a>Rechercher</a>
+    </footer>
+    
+</body>
+</html>
+
