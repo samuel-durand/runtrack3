e1bec5be844c2299af963e363e7252551687ac5b
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-04 11:50:29 +0200

    style tailwind

---
+++ jour08/job05/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Document</title>
+</head>
+<body>
+
+<header class="flex justify-end bg-blue h-24 items-center">
+    <h1 class="mr-auto">LOGO</h1>
+    <nav class="items-center">
+        <ul class="flex space-evenly">
+            <li><a href="" class="px-4 py-2">Use Case</a></li>
+            <li><a href="" class="px-4 py-2">for business</a></li>
+            <li><a href="" class="px-4 py-2">Resource</a></li>
+            <li><a href="" class="px-4 py-2">company</a></li>
+            <li><a href="" class="px-4 py-2">pricing</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+
+<main>
+    <section class="bg-purple-100 min-h-screen flex flex-col justify-center items-center">
+        <h1 class="text-4xl font-bold mb-4 text-left">Loom on Meeting off.</h1>
+        <p class="text-xl text-left">Record quick down meeting by 29%</p>
+
+        <button class="mt-8 bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600"> Get Loom for free</button>
+
+    </section>
+</main>
+
+    
+</body>
+</html>
