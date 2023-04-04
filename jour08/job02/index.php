3271828307163ad2ee7b6650fa0c1996b96d43c9
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-04 11:25:21 +0200

    done

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -2,58 +2,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <title>job02</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <style type="text/tailwindcss"></style>
-
-  </style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.17/tailwind.min.css">
 </head>
 <body>
-    <header>
-        <nav class="flex justify-evenly bg-gradient-to-r from-purple-500 to-pink-500 w-100 h-16 text-white" >
-            <a class="mt-4 mx-8 cursor-pointer ">Acceuil</a>
-            <a class="mt-4 mx-8 cursor-pointer" >Inscription</a>
-            <a class="mt-4 mx-8 cursor-pointer">Connexion</a>
-            <a class="mt-4 mx-8 cursor-pointer">Rechercher</a>
+    <header class="bg-gradient-to-r from-purple-500 to-pink-500 fixed top-0 left-0 right-0 h-16 text-white">
+        <nav class="flex justify-evenly h-full items-center">
+            <a class="cursor-pointer">Accueil</a>
+            <a class="cursor-pointer">Inscription</a>
+            <a class="cursor-pointer">Connexion</a>
+            <a class="cursor-pointer">Rechercher</a>
         </nav>
- 
     </header>
-
-        <form >
-            <label for="name">entrer votre nom</label><br>
-            <input type="text" placeholder="nom" required><br>
-
-            <label for="prénom">votre prénom</label><br>
-            <input type="text" placeholder="prénom" required><br>
-
-            <label for="adresse">votre adresse</label><br>
-            <input type="text" placeholder="adresse" required><br>
-
-            <label for="password">votre mot de pass</label><br>
-            <input type="password" placeholder="mot de pass" required><br>
-
-            <label for="homme">Homme</label><br>
-            <input type="radio" name="homme" value="homme">
-
-            <label for="Femme">Femme</label><br>
-            <input type="radio" name="Femme" value="Femme">
-
-            <label>vos passions ! :</label>
-
-            <input type="checkbox">
-            <label for="informatique">informatique</label>
-
-            <input type="checkbox">
-            <label for="voyage">voyage</label>
-
-            <input type="checkbox">
-            <label for="sport">sport</label>
-            <br>
-            <input type="submit" value="envoyer !">
-            <br>
+    <main class="container mx-auto my-16 max-w-md">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="name">Entrer votre nom :</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Nom" required>
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="prenom">Entrer votre prénom :</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="prenom" type="text" placeholder="Prénom" required>
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="adresse">Entrer votre adresse :</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="adresse" type="text" placeholder="Adresse" required>
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2" for="password">Entrer votre mot de passe :</label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Mot de passe" required>
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-bold mb-2">Sexe :</label>
+                <div class="mt-2">
+                    <label class="inline-flex items-center">
+                        <input type="radio" class="form-radio" name="sexe" value="homme">
+                        <span class="ml-2">Homme</span>
+                    </label>
+                    <label class="inline-flex items-center ml-6">
+                        <input type="radio" class="form-radio" name="sexe" value="femme">
+                        <span class="ml-2">Femme</span>
+                    </label>
+                </div>
+            </div>
+            <div class="mb-4">
+                <label for="inline-flex items-center ml-6">
+                <input type="radio" name="passion" value="infomatique">
+                <span>infomatique</span>
+                <input type="radio" name="passion" value="sport">
+                <span>sport</span>
+                </label>
+            </div>
+        </form>
+</main>
 
     <footer class="flex items-center justify-evenly	 bg-gradient-to-r from-purple-500 to-pink-500  absolute inset-x-0 bottom-0 h-16 text-white ">
         <a class="cursor-pointer">Acceuil</a>
